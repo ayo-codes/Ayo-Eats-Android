cdaa6741c8426e565856e72c8651d398bab5d161
Ayo Onamusi <onamusia@gmail.com>
2023-11-02 23:28:03 +0000

    added markers to show list of locations stored on the app

--- app/src/main/java/org/wit/ayoeats/activities/MealLocationMapsActivity.kt
+++ app/src/main/java/org/wit/ayoeats/activities/MealLocationMapsActivity.kt
@@ -2,21 +2,76 @@ package org.wit.ayoeats.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.ayoeats.databinding.ActivityMealLocationMapsBinding
+import org.wit.ayoeats.databinding.ContentMealLocationMapsBinding
+import org.wit.ayoeats.main.MainApp
 
 class MealLocationMapsActivity : AppCompatActivity() {
 
-
-    private lateinit var binding: ActivityMealLocationMapsBinding
+    private lateinit var binding: ActivityMealLocationMapsBinding // binds to the outer layout
+    private lateinit var contentBinding: ContentMealLocationMapsBinding // binds to the content meal location layout
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        app = application as MainApp
+
         binding = ActivityMealLocationMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+
+        contentBinding = ContentMealLocationMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState) // creates the map in the mapView
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
 
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+
+    private fun configureMap(){
+        map.uiSettings.isZoomControlsEnabled = true
+        app.mealLocations.findAll().forEach{
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.mealName).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+
+    }
 }
+
+
